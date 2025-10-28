084490313b784411146d072d55673cc71a9f2237
Morad Djaoui <moradjaoui@gmail.com>
2025-10-28 05:28:20 +0100

    fix: use only generateMetadata (add fc:miniapp meta)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,21 +2,15 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { miniapp } from './lib/miniapp';
 
-export const metadata: Metadata = {
-  // metadataBase pour générer des URL absolues si besoin
-  metadataBase: new URL(miniapp.homeUrl),
-  title: miniapp.name,
-  description: miniapp.description,
-};
-
 export async function generateMetadata(): Promise<Metadata> {
   return {
+    metadataBase: new URL(miniapp.homeUrl),
     title: miniapp.name,
     description: miniapp.description,
     other: {
       'fc:miniapp': JSON.stringify({
         version: miniapp.version,
-        imageUrl: miniapp.heroImageUrl, // 3:2 recommandé (ex: 1200x800)
+        imageUrl: miniapp.heroImageUrl, // ratio 3:2 recommandé
         button: {
           title: `Open ${miniapp.name}`,
           action: {
